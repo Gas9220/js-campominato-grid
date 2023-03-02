5c5782e9d9d8be431a3d9d80b0e93a2c598f40ea
Gaspare <Gas9220@users.noreply.github.com>
2023-03-02 16:51:09 +0100

    Aggiunti i riferimenti HTML e il livello default

---
+++ js/main.js
@@ -0,0 +1,8 @@
+'use strict';
+
+// Riferimenti HTML
+const containerElement = document.querySelector('.container');
+const playBtn = document.getElementById('play-btn');
+
+// Livello di default
+let level = "easy";
