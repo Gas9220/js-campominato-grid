611b511411271f8cda5a1e3a4fd44ac88828cdf7
Gaspare <Gas9220@users.noreply.github.com>
2023-03-02 16:51:42 +0100

    Creata la funzione per creare i box

--- js/main.js
+++ js/main.js
@@ -1,5 +1,20 @@
 'use strict';
 
+// ---------- FUNCTIONS ----------
+function createBox(boxTextContent) {
+    const boxElement = document.createElement('div');
+    boxElement.classList.add('box');
+
+    const boxSpanElement = document.createElement('span');
+    boxSpanElement.innerHTML = boxTextContent
+
+    boxElement.append(boxSpanElement);
+
+    return boxElement
+}
+
+// -------------------------------
+
 // Riferimenti HTML
 const containerElement = document.querySelector('.container');
 const playBtn = document.getElementById('play-btn');
